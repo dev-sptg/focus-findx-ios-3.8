5003bb18e67af1a12a4ffab451b24fb964946d4c
Maryan <shkurdamarjan@gmail.com>
2017-09-21 18:33:31 +0300

    Init

---
+++ Blockzilla/GradientBackgroundView.swift
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+
+class GradientBackgroundView: UIView {
+    init(alpha: Float = 0.1, startPoint: CGPoint = CGPoint(x: -0.2, y: 0), endPoint: CGPoint = CGPoint(x: 1.2, y: 1)) {
+        super.init(frame: CGRect.zero)
+
+        backgroundColor = UIConstants.colors.gradientBackground
+
+        let gradient = self.layer as! CAGradientLayer
+        gradient.startPoint = startPoint
+        gradient.endPoint = endPoint
+        let gradients = [UIConstants.colors.gradientLeft, UIConstants.colors.gradientMiddle, UIConstants.colors.gradientRight]
+        gradient.colors = gradients.map { $0.withAlphaComponent(CGFloat(alpha)).cgColor }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+}
